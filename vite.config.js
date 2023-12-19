9f9103a4d4764e6f75b030eea37eafc2ae0f0552
Anderson Santos Da Silva Filho <50058700+Ander-Tk@users.noreply.github.com>
2023-12-19 17:08:13 -0300

    Utilizando plugin vite-pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({ registerType: 'autoUpdate',
+    manifest: {
+      name: 'React Tasks',
+      short_name: 'React Tasks',
+      description: 'Uma simples lista de tarefas com React',
+      theme_color: '#ffffff',
+      icons: [{ src: './src/icones/tasks-192.png', sizes: '192x192', type: 'image/png' }]
+    }
+  }
+  )],
 })
