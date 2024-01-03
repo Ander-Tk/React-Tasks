ea45f750b7d714cb6be1a30bcad85d6e7c621f85
Anderson Santos Da Silva Filho <50058700+Ander-Tk@users.noreply.github.com>
2024-01-03 09:40:42 -0300

    PWA Completa

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,14 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate',
+  build: { outDir: 'dist', assetsDir: 'public' },
+  plugins: [react(), VitePWA({ registerType: 'autoUpdate', devOptions: { enabled: true },
     manifest: {
       name: 'React Tasks',
       short_name: 'React Tasks',
       description: 'Uma simples lista de tarefas com React',
       theme_color: '#ffffff',
-      icons: [{ src: './src/icones/tasks-192.png', sizes: '192x192', type: 'image/png' }]
+      icons: [{ src: './icones/tasks-192.png', sizes: '192x192', type: 'image/png' }]
     }
   }
   )],
